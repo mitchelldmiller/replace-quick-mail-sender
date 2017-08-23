02b11eae2e594ac408c240e51d1520319a087b28
Mitchell D. Miller <mitch@wheredidmybraingo.com>
2017-08-23 15:30:21 -0400

    first public version

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# replace-quick-mail-sender
+Replace Quick Mail Sender
+====================
+
 WordPress Plugin to replace user sender name and address in Quick Mail plugin.
+
+Description
+-----------
+
+![screenshot](screenshot.png)
+
+
+Implements filter for current user to replace their sending name and address.
+
+Requires Administrator capabilities and [Quick Mail plugin](https://github.com/mitchelldmiller/quick-mail-wp-plugin) 3.2.0 or later.
+
+__Features__
+
+* Includes `replace_quick_mail_sender` filter, and form to change sending name and address with it. 
+
+### Learn More
+
+* See [Send Reliable Email from WordPress with Quick Mail](https://wheredidmybraingo.com/send-reliable-email-wordpress-quick-mail/).
+
+### Installation ###
+
+* Download [the latest release](https://github.com/mitchelldmiller/replace-quick-mail-sender/releases/latest) and unpack in your `/wp-content/plugins/` directory.
+
+* Verify that Quick Mail plugin is activated.
+
+* Activate the plugin through the WordPress 'Plugins' menu.
+
+### Frequently Asked Questions ###
+
+__Who can use this plugin?__
+
+* User must be an Administrator. Site must have Quick Mail plugin active.
+
+__How to Disable Filter__
+
+* Delete name or email, to temporarily disable filter.
+
+__Saves Credentials__
+
+* Does not delete user info when plugin is uninstalled.
+
+__Limitations__
+
+* Check `From` name in Quick Mail, because other mail plugins can override sender credentials.
+
+* Replacement name must be 1-80 characters.
+
+* Replacement email address is verified if `Verify recipient email domains` is enabled.
+
+__Translators and Programmers__
+
+* A .pot file is included for translators.
+
+__License__
+
+This plugin is free for personal or commercial use. 
---
+++ replace-quick-mail-sender.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ Plugin Name:  Replace Quick Mail Sender
+ Description:  Use replace_quick_mail_sender filter to replace Quick Mail name and sender. Does not work if another mail plugin is overriding credentials.
+ Version:      0.0.3
+ Author:       Mitchell D. Miller
+ Author URI:   https://wheredidmybraingo.com/
+ Plugin URI:   https://wheredidmybraingo.com/send-reliable-email-wordpress-quick-mail/
+ Text Domain:  quick-mail-sender
+ Domain Path:  /lang
+ License:      GPLv3
+ License URI:  https://www.gnu.org/licenses/lgpl.html
+ */
+
+class ReplaceQuickMailSender {
+	public static $qm_dir;
+
+	public function __construct() {
+		if ( ! function_exists( 'register_activation_hook' ) ) {
+			exit;
+		} // end if not WordPress
+
+		add_filter( 'replace_quick_mail_sender', array($this, 'replace_quick_mail_sender') );
+		add_action( 'activated_plugin', array($this, 'install_quick_mail_filters'), 10, 0);
+		add_action( 'admin_menu', array($this, 'init_quick_mail_sender_menu') );
+		add_action( 'plugins_loaded', array($this, 'init_quick_mail_translation') );
+	} // end constructor
+
+	/**
+	 * install Quick Mail filter. requires Quick Mail Plugin >= 3.2.0
+	 *
+	 */
+	public function install_quick_mail_filters() {
+		if ( !class_exists( 'QuickMail' ) ) {
+			deactivate_plugins( basename( __FILE__ ) );
+			echo sprintf("<div class='notice notice-error' role='alert'>%s</div>",
+					__( 'Requires Quick Mail plugin.', 'quick-mail-sender' ) );
+			exit;
+		} // end if no quick mail
+
+		$qm = QuickMail::get_instance();
+		self::$qm_dir = empty( $qm->directory ) ? '/' : $qm->directory;
+		$want = self::$qm_dir . 'inc/qm_util.php';
+		unset( $qm );
+		if ( !file_exists( $want ) ) {
+			deactivate_plugins( basename( __FILE__ ) );
+			echo sprintf("<div class='notice notice-error' role='alert'>%s</div>",
+					__( 'Requires Quick Mail 3.2.0 or later', 'quick-mail-sender' ) );
+			exit;
+		} // end if
+
+		$uid = get_current_user_id();
+		if ( $uid != get_user_meta( $uid, 'qmf_quick_mail_user', true ) ) {
+			$qm_options = array('qmf_quick_mail_user' => $uid, 'qmf_quick_mail_email' => '', 'qmf_quick_mail_name' => '');
+			foreach ($qm_options as $k => $v) {
+				update_user_meta( $uid, $k, $v );
+			} // end foreach
+		} // end if not same user who installed plugin
+	} // install_quick_mail_filters
+
+	/**
+	 * load translations.
+	 */
+	public function init_quick_mail_translation() {
+		load_plugin_textdomain( 'quick-mail-sender', false, basename( dirname( __FILE__ ) ) . '/lang' );
+	} // end init_quick_mail_translation
+
+	/**
+	 * init admin menu for appropriate users.
+	 */
+	public function init_quick_mail_sender_menu() {
+		$title = __( 'Quick Mail Sender', 'quick-mail-sender' );
+		$min_permission = 'activate_plugins';
+		$link = 'edit_quick_mail_sender';
+		$page = add_options_page( $title, $title, $min_permission, $link, array($this, $link) );
+		add_action( 'admin_print_styles-' . $page, array($this, 'init_quick_mail_style') );
+	} // end init_quick_mail_sender_menu
+
+	/**
+	 * use by admin print styles to add Quick Mail plugin css to admin.
+	 */
+	public function init_quick_mail_style() {
+		if ( !class_exists( 'QuickMail' ) ) {
+			echo sprintf("<div class='notice notice-error' role='alert'>%s</div>",
+					__( 'Requires Quick Mail plugin.', 'quick-mail-sender' ) );
+			exit;
+		} // end if
+		if ( empty( self::$qm_dir ) ) {
+			$qm = QuickMail::get_instance();
+			self::$qm_dir = empty( $qm->directory ) ? '/' : $qm->directory;
+			unset( $qm );
+		} // end if lost value
+		$file = self::$qm_dir . 'lib/css/quick-mail.css';
+		$path = str_replace( $_SERVER['DOCUMENT_ROOT'], '', $file );
+		wp_enqueue_style( 'quick-mail', $path, array(), null, 'all' );
+	} // end init_quick_mail_style
+
+	public function edit_quick_mail_sender() {
+		$message = '';
+		$uid = get_current_user_id();
+		$showform = ( $uid == get_user_meta( $uid, 'qmf_quick_mail_user', true ) );
+		$email = $showform ? get_user_meta( $uid, 'qmf_quick_mail_email', true ) : '';
+		$name = $showform ? get_user_meta( $uid, 'qmf_quick_mail_name', true ) : '';
+		if ( $showform && 'POST' == $_SERVER['REQUEST_METHOD'] ) {
+			if ( empty( self::$qm_dir ) ) {
+				$qm = QuickMail::get_instance();
+				self::$qm_dir = empty( $qm->directory ) ? '/' : $qm->directory;
+				unset( $qm );
+			} // end if lost value
+
+			require_once self::$qm_dir . 'inc/qm_util.php';
+			$uname = empty($_POST['qmf_quick_mail_name']) ? '' : trim( $_POST['qmf_quick_mail_name'] );
+			if ( !empty( $uname ) && !QuickMailUtil::check_char_count( $uname ) ) {
+				$message = __( 'Invalid or missing Name', 'quick-mail-sender' );
+			} // end if invalid name
+
+			$umail = empty($_POST['qmf_quick_mail_email']) ? '' : sanitize_email( trim( $_POST['qmf_quick_mail_email'] ) );
+			$verify_domain = '';
+			if ( is_multisite() ) {
+				$verify_domain = get_blog_option( get_current_blog_id(), 'verify_quick_mail_addresses', 'N' );
+			} else {
+				$verify_domain = get_option( 'verify_quick_mail_addresses', 'N' );
+			} // end if multisite
+			if ( empty( $message ) && !empty( $umail ) && !QuickMailUtil::qm_valid_email_domain( $umail, $verify_domain ) ) {
+				$message = __( 'Invalid Email Address', 'quick-mail-sender' );
+			} // end if invalid email
+
+			if ( empty( $message) ) {
+				$updated = false;
+				if ( $uname != $name ) {
+					update_user_meta( $uid, 'qmf_quick_mail_name', $uname );
+					$name = $uname;
+					$updated = true;
+				} // end if updated name
+
+				if ( $umail != $email ) {
+					update_user_meta( $uid, 'qmf_quick_mail_email', $umail );
+					$email = $umail;
+					$updated = true;
+				} // end if updated email
+
+				if ( ( empty( $email ) || empty( $name ) ) && $updated) {
+					echo '<div role="alert" class="updated"><p>', _e( 'Filter Disabled', 'quick-mail-sender' ), '</p></div>';
+				} elseif ($updated) {
+					echo '<div role="alert" class="updated"><p>', _e( 'Option Updated', 'quick-mail-sender' ), '</p></div>';
+				}
+			} else {
+				echo "<div class='notice notice-error' role='alert'>{$message}</div>";
+			} // end if no error message
+		} // end if post
+?>
+<h1 id="quick-mail-title" class="quick-mail-title"><?php _e( 'Replace Quick Mail Sender', 'quick-mail-sender' ); ?></h1>
+<div id="poststuff">
+	<div id="post-body-content">
+		<?php if ( $showform ) : ?>
+		<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+		<fieldset>
+		<label id="te_label" for="qmf_quick_mail_name" class="recipients"><?php _e( 'Name', 'quick-mail-sender' ); ?></label>
+		<p><input type="text" aria-labelledby="te_label" size="64" maxlength="80" value="<?php echo $name; ?>" name="qmf_quick_mail_name" id="qmf_quick_mail_name" tabindex="1"></p>
+		<label id="tn_label" for="qmf_quick_mail_email" class="recipients"><?php _e( 'Email', 'quick-mail-sender' ); ?></label>
+		<p><input type="email" aria-labelledby="tn_label" size="64" maxlength="255" value="<?php echo $email; ?>" name="qmf_quick_mail_email" id="qmf_quick_mail_email" tabindex="2"></p>
+<p class="submit"><input type="submit" id="qm-submit" name="qm-submit"
+title="<?php _e( 'Update', 'quick-mail-sender' ); ?>" tabindex="99"
+value="<?php _e( 'Update', 'quick-mail-sender' ); ?>"></p>
+		</fieldset>
+		</form>
+		<?php else : ?>
+		<p><?php _e( 'You are not authorized to use this plugin.', 'quick-mail-sender' ); ?></p>
+		<?php endif; ?>
+	</div>
+</div>
+<?php
+	} // end edit_quick_mail_sender
+
+	/**
+	 * replace quick mail sender.
+	 * @param array $args $args['email'] = email, $args['name'] = name
+	 * @return array modified name, email
+	 */
+	public function replace_quick_mail_sender( $args ) {
+		$uid = get_current_user_id();
+		if ($uid == get_user_meta( $uid, 'qmf_quick_mail_user', true ) ) {
+			$email = get_user_meta( $uid, 'qmf_quick_mail_email', true );
+			$name = get_user_meta ($uid, 'qmf_quick_mail_name', true );
+			if ( !empty( $email ) && !empty( $name ) ) {
+				return array('email' => $email, 'name' => $name);
+			} // end if got name and email
+		} // end if same user is checking
+
+		return $args;
+	} // end replace_quick_mail_sender
+} // end class
+new ReplaceQuickMailSender;
